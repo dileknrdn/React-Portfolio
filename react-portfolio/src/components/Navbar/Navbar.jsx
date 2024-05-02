4f58557ef30f2d1d58f87dd76675478804da40ba
Dilek Nuredin <dileknuredin@hotmail.com>
2024-05-02 02:44:07 -0400

    added style to navbar

--- react-portfolio/src/components/Navbar/Navbar.jsx
+++ react-portfolio/src/components/Navbar/Navbar.jsx
@@ -1,28 +1,45 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import styles from "./Navbar.module.css";
-import {getImageUrl} from "../../utils";
+import { getImageUrl } from "../../utils";
 
 export const Navbar = () => {
-    return <nav ClassName={styles.navbar}>
-        <a ClassName={styles.title} href="/">Portfolio</a>
-        <div ClassName={styles.menu}>
-            <ul ClassName={styles.menuItems}>
-                <li>
-                    <a href="#about">About</a>
-                </li>
-                <li>
-                    <a href="#projects">Projects</a>
-                </li>
-                <li>
-                    <a href="#contact">Contact</a>
-                </li>
-                <li>
-                    <a href="#resume">Resume</a>
-                </li>
-            </ul>
+  const [menuOpen, setMenuOpen] = useState(false);
 
-
-        </div>
-    </nav>;
+  return (
+    <nav className={styles.navbar}>
+      <a className={styles.title} href="/">
+        Portfolio
+      </a>
+      <div className={styles.menu}>
+        <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageUrl("nav/closeIcon.png")
+              : getImageUrl("nav/menuIcon.png")
+          }
+          alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <ul
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(false)}
+        >
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+          <li>
+            <a href="#resume">Resume</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
